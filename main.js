71bcb313189fd070b947a956c4b6466a6a2a233d
Keanan083 <philanisithembiso@gmail.com>
2020-04-05 23:09:54 +0200

    this is my HTML,CSS,BOOTSTRAP project

---
+++ main.js
@@ -0,0 +1,13 @@
+var more = document.getElementById("more")
+more.onclick = function () {
+    alert("This is the only staff we have...");
+}
+
+
+
+if the_1st🚦 == 🛑 && the_2nd🚦 == 💚{
+    alert("better");
+}
+else {
+    alert("why are you being so hard to me...?")
+}
